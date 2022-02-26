bce37b850df75bc8435efe15d8c37263295c6636
Krishanth1 <krishanthchandrakumarkottala@gmail.com>
2022-02-27 01:41:46 +0530

    second mint

--- mysite/urls.py
+++ mysite/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')), #add this
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='main/password/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/password/password_reset_confirm.html"), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='main/password/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 if settings.DEBUG: #add this
